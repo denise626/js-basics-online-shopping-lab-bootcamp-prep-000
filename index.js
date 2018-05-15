7e4237fc602352c2fa7b7ef562c2efd0587f24f0
April Kiser <denise626@hotmail.com>
2018-05-15 01:00:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,13 +82,11 @@ function sumOfAllPrices() {
   return sum
 }
 
-function notifyUserThereIsNoItemToRemove() {
+function tellCustomerNoRemovableItemsInCart() {
   return 'That item is not in your cart.'
 }
 
 function removeItemFromCart(itemToRemove) {
   var indexOfItemToRemove = cart.indexOf(itemToRemove)
-  //Array.prototype.splice()
-  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
   getCart().splice(indexOfItemToRemove,1)
 }
